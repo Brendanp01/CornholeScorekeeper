7a31f6d5dc9942fce82b092a600f627add84ad66
Brendan Pittman <brendanp01@outlook.com>
2023-01-30 11:46:32 -0500

    Update

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var redInHole = 0;
 var blueScore = 0;
 var redScore = 0;
 
+function updateScore() {
+    document.getElementById("bluescore").innerHTML = blueScore;
+    document.getElementById("redscore").innerHTML = redScore;
+}
 function reset() {
     blueOnBoard = 0;
     redOnBoard = 0;
@@ -24,7 +28,7 @@ function blueHole(amount) {
 function redHole(amount) {
     redInHole = amount;
 }
-function nextRound() {
+function calculateScore() {
     if (blueOnBoard + (blueInHole * 3) > redOnBoard + (redInHole * 3)) {
         blueScore = blueScore + ((blueOnBoard + (blueInHole * 3)) - (redOnBoard + (redInHole * 3)));
         reset();
@@ -34,11 +38,33 @@ function nextRound() {
     } else {
         reset();
     }
-    document.getElementById("bluescore").innerHTML = blueScore;
-    document.getElementById("redscore").innerHTML = redScore;
+}
+function nextRound() {
+    if(blueOnBoard + blueInHole <= 4 && redOnBoard + redInHole <= 4) {
+        calculateScore();
+        updateScore();
+    } else {
+        alert("A team cannot have more than four bags.");
+        reset();
+    }
 }
 window.onload = function() {
     if(/iP(hone|ad)/.test(window.navigator.userAgent)) {
         document.body.addEventListener('touchstart', function() {}, false);
+    }
+}
+
+function setBlueScore() {
+    score = Number(prompt("Enter a score for blue team", blueScore));
+    if (blueScore != NaN) {
+        blueScore = score;
+        updateScore();
+    }
+}
+function setRedScore() {
+    score = Number(prompt("Enter a score for red team", redScore));
+    if (redScore != NaN) {
+        redScore = score;
+        updateScore();
     }
 }
